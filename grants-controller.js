16a1a0a06dbed103276934c03418b739dbe444f4
mamund <mamund@yahoo.com>
2012-03-12 01:16:20 +0000

    'update model doc'

--- grants-controller.js
+++ grants-controller.js
@@ -1,4 +1,38 @@
 /* miss grants controller media type model */
+
+// DSL model from Martin Fowler's book:
+// "Domain Specific Languages"
+
+/* processing rules:
+
+  href
+    this is the URI of the current representation
+
+  possible-states
+    this is the list of states the service
+    decided to expose and the possible valid
+    values that user-agents can use.
+
+  current-states
+    this is the list of current states
+    (for the connected user-agent).
+
+  actions
+    this is the list of possible actions
+    the user-agent can use to manipulate
+    the state of the service. note that
+    the model shows all the _possible_
+    actions, but user-agents will only
+    see the ones valid for that user-agent
+    at the momemnt (depending on the state
+    of the service)
+
+    activating the state is done as follows:
+    - use HTTP.PUT
+    - use x-www-form-urlencoded
+    - use the href as the target URI
+    - serialize body exactly (name=door&value=closed)
+*/
 {
   "href" : "...",
   "possible-states" :
